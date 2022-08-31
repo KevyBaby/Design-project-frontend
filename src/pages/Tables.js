36d0c6823bd6507b331c562ed00ae110944b9db7
KevyBaby <52975654+KevyBaby@users.noreply.github.com>
2022-08-31 21:53:12 +0200

    Add files via upload

---
+++ src/pages/Tables.js
@@ -0,0 +1,110 @@
+import './Tables.css';
+
+function Tables() {
+    return (   
+      <div className="App">
+          <table>
+              <tr>
+              <th>Driver Full Names</th>
+              <th>Email</th>
+              <th>Car Model</th>
+              <th>Travel Date</th>
+              <th>Route Hours</th>
+              <th>Kilometers</th>
+              </tr>
+              <tr>
+              <td>Alex Gordons</td>
+              <td>alexa@gmail.com</td>
+              <td>Mahindra</td>
+              <td>27-08-2022</td>
+              <td>03:57</td>
+              <td>48.1KM</td>
+              </tr>
+              <tr>
+              <td>Nkosi Mdluli</td>
+              <td>nkosi101@gmail.com</td>
+              <td>Volvo</td>
+              <td>26-08-2022</td>
+              <td>04:44</td>
+              <td>30.0KM</td>
+              </tr>
+              <tr>
+              <td>Jacob Lans</td>
+              <td>jpeterson@gmail.com</td>
+              <td>Volvo</td>
+              <td>24-08-2022</td>
+              <td>06:23</td>
+              <td>51.6KM</td>
+              </tr>
+              <tr>
+              <td>Peter Merwe</td>
+              <td>petermerwe@gmail.com</td>
+              <td>Man</td>
+              <td>24-08-2022</td>
+              <td>04:50</td>
+              <td>43.5KM</td>
+              </tr>
+              <tr>
+              <td>Katlego Diole</td>
+              <td>katdiole@gmail.com</td>
+              <td>Man</td>
+              <td>22-08-2022</td>
+              <td>04:38</td>
+              <td>35.2KM</td>
+              </tr>
+              <tr>
+              <td>Solomon Bogatsu</td>
+              <td>Solomon60@gmail.com</td>
+              <td>Man</td>
+              <td>24-08-2022</td>
+              <td>04:18</td>
+              <td>36.8KM</td>
+              </tr>
+              <tr>
+              <td>Jan Van Dyk</td>
+              <td>jvandyk@gmail.com</td>
+              <td>Man</td>
+              <td>22-08-2022</td>
+              <td>05:30</td>
+              <td>51.5KM</td>
+              </tr>
+              <tr>
+              <td>Danny Reeves</td>
+              <td>danreeves@gmail.com</td>
+              <td>Mercedes</td>
+              <td>20-08-2022</td>
+              <td>04:48</td>
+              <td>34.4KM</td>
+              </tr>
+              <tr>
+              <td>Kyle Paige</td>
+              <td>kylep@gmail.com</td>
+              <td>Mahindra</td>
+              <td>21-08-2022</td>
+              <td>05:52</td>
+              <td>47.7KM</td>
+              </tr>
+              <tr>
+              <td>Ronald Mpofu</td>
+              <td>ronaldmpofu@gmail.com</td>
+              <td>Man</td>
+              <td>20-08-2022</td>
+              <td>06:25</td>
+              <td>49.0KM</td>
+              </tr>
+              <tr>
+              <td>Hector Modise</td>
+              <td>modisehector@gmail.com</td>
+              <td>Man</td>
+              <td>20-08-2022</td>
+              <td>05:33</td>
+              <td>50.6KM</td>
+              </tr>
+
+          </table>
+      </div>
+  );
+}
+
+export default Tables
+;
