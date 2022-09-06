bdd721b0777932b9d21cbf385209fec8c6ae0a1c
KevyBaby <52975654+KevyBaby@users.noreply.github.com>
2022-09-06 10:31:23 +0200

    Analytics

---
+++ src/src/pages/Drivers.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export const Drivers = () => {
+    return (
+        <main>
+            <Link to="/">Back</Link><br />
+            <h2>Welcome to the Drivers!</h2>
+        </main>
+    )
+}
